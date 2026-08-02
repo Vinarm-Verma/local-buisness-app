6b181470596d4b3227ff2b22b87323d2a35ac874
Vinarm verma <vinarmverma95@gmail.com>
2026-08-02 21:46:23 +0530

    Complete backend for all Zomato-style features

--- models/Item.js
+++ models/Item.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-  businessId: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+  businessId: { type: mongoose.Schema.Types.ObjectId, ref: "Business" },
   name: String,
   description: String,
   price: Number,
+  unit: { type: String, default: "item" },
   photo: String,
   available: { type: Boolean, default: true }
 });
 
-module.exports = mongoose.model('Item', itemSchema);
+module.exports = mongoose.model("Item", itemSchema);
